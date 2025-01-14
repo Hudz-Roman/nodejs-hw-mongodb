496948b9324b4d1e7b63ff9388d21f72f405d1c9
Roman Hudz <hudzrom@gmail.com>
2025-01-14 21:00:09 +0200

    HW done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-console.log(setupServer());
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
@@ -21,9 +22,37 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('*', (req, res, next) => {
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use('*', (req, res) => {
     res.status(404).json({
-      message: 'Not found',
+      message: 'Contact not found',
     });
   });
 
