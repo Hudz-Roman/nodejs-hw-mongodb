0eb416aa7263a43f0cf0f79904a3ec69ab4ca9df
Roman Hudz <hudzrom@gmail.com>
2025-01-19 15:24:26 +0200

    Create POST

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import {
   getContactByIdController,
   getContactsController,
+  createContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
+
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
 
 export const contactsRouter = router;
