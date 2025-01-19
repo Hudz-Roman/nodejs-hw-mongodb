da488cff685c662186fa207e4ac70c06cd9cfbde
Roman Hudz <hudzrom@gmail.com>
2025-01-19 16:40:07 +0200

    Create PATCH

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,24 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
   return contacts;
