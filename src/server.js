a2d33f9827fc77cdf8013087ce8dd1643158299e
Roman Hudz <hudzrom@gmail.com>
2025-02-24 18:20:06 +0200

    Create swagger get + post

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { indexRouter } from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ export const setupServer = () => {
   app.use(indexRouter);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
